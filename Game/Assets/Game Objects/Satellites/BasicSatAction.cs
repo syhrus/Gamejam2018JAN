751165fc73197008afd25e6e3f799db1ea1eeada
TenDrunkMonks <35618043+TenDrunkMonks@users.noreply.github.com>
2018-01-27 16:33:10 +1100

    Level tweaks, colours for satellite filter effects.

--- Game/Assets/Game Objects/Satellites/BasicSatAction.cs
+++ Game/Assets/Game Objects/Satellites/BasicSatAction.cs
@@ -12,19 +12,39 @@ public class BasicSatAction : MonoBehaviour {
 	void Start()
 	{	
 		switch (audioEffect) {
+		case AudioManager.EffectType.CLEAN:
+			{	
+				GetComponent<Light> ().color = new Color (0.4f, 0.4f, 0.4f); //grey
+				break;
+			}
+		case AudioManager.EffectType.RINGMOD:
+			{	
+				GetComponent<Light> ().color = new Color (0.8f, 0f, 0.8f); //hot pink
+				break;
+			}
+		case AudioManager.EffectType.FLANGE:
+			{	
+				GetComponent<Light> ().color = new Color (0f, 1f, 0f); //green
+				break;
+			}
 		case AudioManager.EffectType.BITCRUSH:
 			{	
-				GetComponent<Light> ().color = new Color (0.8f, 0f, 0f);
+				GetComponent<Light> ().color = new Color (0.8f, 0f, 0f); //red
 				break;
 			}
 		case AudioManager.EffectType.SQUELCH:
 			{
-				GetComponent<Light> ().color = new Color (0.768f, 0.768f, 0.0784f);
+				GetComponent<Light> ().color = new Color (0.8f, 0.4f, 0.1f); //orange
 				break;
 			}
 		case AudioManager.EffectType.REVERB:
 			{
-				GetComponent<Light> ().color = new Color (0.8f, 0f, 0.8f);
+				GetComponent<Light> ().color = new Color (0.8f, 0.8f, 0f); //yellow
+				break;
+			}
+		case AudioManager.EffectType.VOICE:
+			{	
+				GetComponent<Light> ().color = new Color (0f, 0.3f, 1f); //dark blue
 				break;
 			}
 		}
