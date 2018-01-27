75bd26d326450e508d987b3a9f1cd2440080dce9
syhrus <tugman1234@hotmail.com>
2018-01-27 16:51:08 +1100

    Many Fix
    Doooooooom

--- Game/Assets/Game Objects/Satellites/SatelliteRotate.cs
+++ Game/Assets/Game Objects/Satellites/SatelliteRotate.cs
@@ -15,7 +15,7 @@ public class SatelliteRotate : MonoBehaviour {
     void OnMouseDown()
     {
         isClicked = true;
-        Debug.Log("CLICK");
+        //Debug.Log("CLICK");
     }
 
     private void Update()
@@ -27,10 +27,18 @@ public class SatelliteRotate : MonoBehaviour {
 
         if (isClicked)
         {
-            float MouseDelta = Input.GetAxis("Mouse X");
-            Debug.Log("Change: " + MouseDelta);
+            //float MouseDelta = Input.GetAxis("Mouse X");
+            //Debug.Log("Change: " + MouseDelta);
+            //transform.Rotate(new Vector3(0, 0, -1), MouseDelta * rotateSpeed);
 
-            transform.Rotate(new Vector3(0, 0, -1), MouseDelta * rotateSpeed);
+            Vector3 mouse_pos = Input.mousePosition;
+            mouse_pos.z = 0f; //The distance between the camera and object
+            Vector3 object_pos = Camera.main.WorldToScreenPoint(transform.position);
+            mouse_pos.x = mouse_pos.x - object_pos.x;
+            mouse_pos.y = mouse_pos.y - object_pos.y;
+            float angle = (Mathf.Atan2(mouse_pos.y, mouse_pos.x) * Mathf.Rad2Deg)- 90.0f;
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+
         }
     }
 }
