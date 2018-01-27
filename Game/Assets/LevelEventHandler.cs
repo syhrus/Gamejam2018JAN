bc267c83a6e5963fa18b7877a1eee540099c055f
syhrus <tugman1234@hotmail.com>
2018-01-28 10:02:09 +1100

    LevelEventHandler
    Makes levels work proper good - still need to add persistence

---
+++ Game/Assets/LevelEventHandler.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelEventHandler : MonoBehaviour {
+
+    public int BassEffect = 0;
+    public int SynthEffect = 0;
+    public int HarmonyEffect = 0;
+    public int DrumsEffect = 0;
+    public int Level = 0;
+
+    private void OnEnable()
+    {
+        AudioManager manager = GameObject.Find("Audiomanager").GetComponent<AudioManager>();
+
+        manager.SetVolumeHard(0);
+        manager.fireOnOff = 0;
+        manager.FadeIn(2);
+    }
+}
